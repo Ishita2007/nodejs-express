7229dd7064a7520a3c9068bde46bd8ad711e808a
Ishita <ishbuchi@gmail.com>
2021-06-02 00:35:42 +0530

    build a simple api

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Express Demo app</h1> <h4> Message: Success</h4> <p>version 1.0</p>');
+})
+
+app.get('/products', (req, res) =>{
+    res.send([
+        {
+            productID: '101',
+            price: 100
+        },
+        {
+            productID: '102',
+            price: 150
+        }
+    ])
+
+    })
+
+app.listen(port, ()=>{
+    console.log('Demo app is up and listening to port: 3000');
+    
+})
